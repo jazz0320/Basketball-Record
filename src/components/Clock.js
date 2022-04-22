be7cea64b8b8169ba4e35ed459a6c20ee5543fd1
jazz0320 <jazz19910320@gmail.com>
2022-04-22 10:31:26 +0800

    add reset 24seconds

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,9 +3,10 @@ import { doc, db, setDoc } from "../utils/firebase";
 
 function Clock(props) {
   const [restTime, setRestTime] = useState();
-  const [shotClockRestTime, setShotClockRestTime] = useState(24);
-  const [shotClock, setShotClock] = useState(0);
+  const [shotClockRestTime, setShotClockRestTime] = useState(0);
+  const [shotClock, setShotClock] = useState(24);
   const [timeStop, setTimeStop] = useState(true);
+  const [gameStart, setGameStart] = useState(false);
 
   //timer
   let interval = useRef();
@@ -13,8 +14,26 @@ function Clock(props) {
   let shotClockTimepast = 0;
   const shotClockDuration = 24000;
 
-  const startQuarterTimer = () => {
-    if (props.eachTime) {
+  const action = function (e) {
+    if (e.ctrlKey && e.key === "c") {
+      if (timeStop === true) {
+        startTime();
+      } else if (timeStop === false) {
+        stop();
+      }
+    } else if (e.key === "c") {
+      reset24seconds();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", action);
+  }, []);
+
+  const startTime = function () {
+    if (gameStart === false) {
+      setGameStart(true);
+      alert("開始");
       setTimeStop(false);
       interval.current = setInterval(() => {
         const duration = props.eachTime * 60 * 1000;
@@ -25,26 +44,61 @@ function Clock(props) {
         const shotClockDistance =
           (shotClockDuration - shotClockTimepast) / 1000;
 
-        timePast += 1000;
-        shotClockTimepast += 1000;
+        timePast += 100;
+        shotClockTimepast += 100;
         setRestTime(distance);
         setShotClockRestTime(shotClockDistance * 1000);
         if ((distance < 0) | (shotClockDistance < 0)) {
           //stop timer
+          console.log("startTTT");
           clearInterval(interval.current);
+          setTimeStop(true);
         } else {
           //update timer
           setShotClock(shotClockDistance);
           props.setTimerMinutes(minutes);
           props.setTimerSeconds(seconds);
         }
-      }, 1000);
-    }
-  };
+      }, 100);
+    } else {
+      if (timeStop === true) {
+        alert("繼續");
+        setTimeStop(false);
+        timePast = 0;
+        shotClockTimepast = 0;
+        interval.current = setInterval(() => {
+          const distance = restTime - timePast;
+          const minutes = Math.floor(
+            (distance % (1000 * 60 * 60)) / (1000 * 60)
+          );
+          const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          timePast += 100;
+          setRestTime(distance);
 
-  const start = function () {
-    alert("開始");
-    startQuarterTimer();
+          console.log("ssssccccc", shotClockTimepast);
+          const shotClockDistance = shotClockRestTime - shotClockTimepast;
+          shotClockTimepast += 100;
+          setShotClockRestTime(shotClockDistance);
+
+          console.log("ssss1", shotClockRestTime);
+          console.log("ssss2", shotClockTimepast);
+          console.log("ssss3", shotClockDistance);
+
+          if ((distance < 0) | (shotClockDistance < 0)) {
+            //stop timer
+            console.log("startCCCC");
+            clearInterval(interval.current);
+            setTimeStop(true);
+          } else {
+            //update timer
+
+            setShotClock(shotClockDistance / 1000);
+            props.setTimerMinutes(minutes);
+            props.setTimerSeconds(seconds);
+          }
+        }, 100);
+      }
+    }
   };
 
   const stop = function () {
@@ -53,33 +107,38 @@ function Clock(props) {
     setTimeStop(true);
   };
 
-  const reStartTime = function () {
-    if (timeStop === true) {
-      setTimeStop(false);
+  const reset24seconds = function () {
+    setShotClockRestTime(24000);
+    setShotClock(24);
+    if (timeStop === false) {
+      console.log("work");
+      clearInterval(interval.current);
+
       timePast = 0;
       shotClockTimepast = 0;
       interval.current = setInterval(() => {
         const distance = restTime - timePast;
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        timePast += 1000;
+        timePast += 100;
         setRestTime(distance);
 
-        const shotClockDistance =
-          (shotClockRestTime - shotClockTimepast) / 1000;
-        shotClockTimepast += 1000;
+        const shotClockDistance = 24000 - shotClockTimepast;
+        shotClockTimepast += 100;
         setShotClockRestTime(shotClockDistance);
 
         if ((distance < 0) | (shotClockDistance < 0)) {
           //stop timer
           clearInterval(interval.current);
+          setTimeStop(true);
         } else {
           //update timer
-          setShotClock(shotClockDistance);
+
+          setShotClock(shotClockDistance / 1000);
           props.setTimerMinutes(minutes);
           props.setTimerSeconds(seconds);
         }
-      }, 1000);
+      }, 100);
     }
   };
 
@@ -188,28 +247,24 @@ function Clock(props) {
             -1
           </button>
         </span>
+        <span>
+          <button onClick={reset24seconds}>重置24秒</button>
+        </span>
       </div>
 
-      <button
-        onClick={() => {
-          start();
-        }}
-      >
-        比賽開始
-      </button>
       <button
         onClick={() => {
           stop();
         }}
       >
-        時間暫停
+        暫停
       </button>
       <button
         onClick={() => {
-          reStartTime();
+          startTime();
         }}
       >
-        時間再開始
+        開始
       </button>
     </div>
   );
